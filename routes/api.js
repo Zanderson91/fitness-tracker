db3b7072e6eda64af1d02a73ce3b1e342b44f0e2
Zack Anderson <zack.anderson2011@gmail.com>
2021-09-22 07:42:50 -0500

    Working through out route..update by id

--- routes/api.js
+++ routes/api.js
@@ -26,3 +26,13 @@ router.post("/api/workouts", (req, res) => {
             res.status(400).json(err);
         });
 })
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true, runValidators: true })
+        .then(Workout => {
+            res.json(Workout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})

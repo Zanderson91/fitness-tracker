1e0450b2f0b2b3bd801031eed36190f2e9d7bb20
Zack Anderson <zack.anderson2011@gmail.com>
2021-09-22 07:57:39 -0500

    completed workout.js

--- models/workout.js
+++ models/workout.js
@@ -8,3 +8,38 @@ const workoutSchema = new Schema({
         default: Date.now,
         required: true,
     },
+
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+
+        weight: {
+            type: Number,
+            trim: true
+        },
+
+        duration: {
+            type: Number,
+            trim: true,
+            required: true
+        },
+
+        distance: {
+            type: Number,
+            trim: true
+        }
+    }],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

58240fcc6899c5b3f7db0189e1fbf02fcb3a3322
Zack Anderson <zack.anderson2011@gmail.com>
2021-09-23 21:54:33 -0500

    deleted console.log from seeder

--- seeders/seed.js
+++ seeders/seed.js
@@ -109,7 +109,6 @@ const workoutSeed = [{
 db.Workout.deleteMany({})
     .then(() => db.Workout.collection.insertMany(workoutSeed))
     .then((data) => {
-        console.log(data.result.n + ' records inserted!');
         process.exit(0);
     })
     .catch((err) => {
